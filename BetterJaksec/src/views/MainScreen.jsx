17b602d46e9ae27c93dbcbac1eaa1a4cb21bf08b
Elias Rinne <elias1rinne@gmail.com>
2026-01-30 15:47:23 +0200

    Add initial mainscreen setup

--- BetterJaksec/src/views/MainScreen.jsx
+++ BetterJaksec/src/views/MainScreen.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 const MainScreen = () => {
+
   return (
     <>
-      {/* Komponentit sisälle, tää o vaa se yleisnäkymä */}
-      <div>MainScreen</div>
+    <h1>Main screen</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/profile_page">Profile</Link>
+          </li>
+          <li>
+            <button>Log out</button>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <Outlet />
+      </div>
     </>
   );
 };
