bcb9e46a4e74acb276b92a9eadd7730dbb041eb9
Elias Rinne <elias1rinne@gmail.com>
2026-02-01 13:48:05 +0200

    Add overal navigation for admin pages

---
+++ BetterJaksec/src/views/admin/AdminView.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+const AdminView = () => {
+  return (
+    <>
+    {/* Joko oma profiili adminille tai sit näin miten nyt reitittää */}
+      <nav style={{margin: "10px"}}>
+        <Link to="/admin" style={{margin: "10px"}}>Main</Link>
+        <Link to="/admin/profile" style={{margin: "10px"}}>Profile</Link>
+        <Link to="/admin/users" style={{margin: "10px"}}>Manage users</Link>
+        <button style={{margin: "10px"}}>Log out</button>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default AdminView;
--- BetterJaksec/src/views/admin/MainScreenAdmin.jsx
+++ BetterJaksec/src/views/admin/MainScreenAdmin.jsx
@@ -1,23 +1,10 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MainScreenAdmin = () => {
   return (
     <>
       <h1>Main screen</h1>
-
-      <nav>
-        <Link to="/profile_page">Profile</Link>
-        <button>Log out</button>
-      </nav>
-
-      <div>
-        <Link to="users">Manage users</Link>
-      </div>
-
-      <div>
-        <Outlet />
-      </div>
     </>
   );
 };
