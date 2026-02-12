d71ea217f0b158c0962b9d254deb3f136a917d68
Elias Rinne <elias1rinne@gmail.com>
2026-02-12 09:18:52 +0200

    Add attendancer marking backbone to teh project

--- BetterJaksec/src/views/admin/AdminView.jsx
+++ BetterJaksec/src/views/admin/AdminView.jsx
@@ -6,6 +6,7 @@ const AdminView = () => {
     <>
     {/* Joko oma profiili adminille tai sit näin miten nyt reitittää */}
       <nav style={{margin: "10px"}}>
+        <Link to="/" style={{margin: "10px"}}>Takas user paikkaa</Link>
         <Link to="/admin" style={{margin: "10px"}}>Main</Link>
         <Link to="/admin/profile" style={{margin: "10px"}}>Profile</Link>
         <Link to="/admin/users" style={{margin: "10px"}}>Manage users</Link>
--- BetterJaksec/src/views/user/AttendanceView.jsx
+++ BetterJaksec/src/views/user/AttendanceView.jsx
@@ -1,10 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const AttendanceView = () => {
+const AttendanceView = ({ course }) => {
+  //** TODO: add apicalls for getting students and posting their attendance to the menu
+  //   Also make this maybe a component or look at admin page how to provide props here*/
+  const dummyUsers = [
+    { studentID: 1, firstName: "Anna", lastName: "Korhonen" },
+    { studentID: 2, firstName: "Mikko", lastName: "Virtanen" },
+    { studentID: 3, firstName: "Laura", lastName: "Mäkinen" },
+    { studentID: 4, firstName: "Jussi", lastName: "Lahtinen" },
+    { studentID: 5, firstName: "Emma", lastName: "Salonen" },
+  ];
+
+  const navigate = useNavigate();
+
+  const [unmarkedStudent, setUnmarkedStudent] = useState([]);
+  const [presentStudent, setPresentStudent] = useState([]);
+
+  const absentToPresent = (student) => {
+    setPresentStudent((prev) => [...prev, student]);
+
+    setUnmarkedStudent((prev) =>
+      prev.filter((s) => s.studentID !== student.studentID),
+    );
+  };
+
+  const presentToAbsent = (student) => {
+    setUnmarkedStudent((prev) => [...prev, student]);
+
+    setPresentStudent((prev) =>
+      prev.filter((s) => s.studentID !== student.studentID),
+    );
+  };
+
+  useEffect(() => {
+    const updateStudentList = () => {};
+  }, [presentStudent, unmarkedStudent]);
+
+  //initial effect
+  useEffect(() => {
+    const initialStudents = () => {
+      // API call here to get course students, can be fetched by course.courseID for example.
+      setUnmarkedStudent(dummyUsers);
+    };
+    initialStudents();
+  }, []);
+
   return (
     <>
-      {/* Komponentit sisälle, tää o vaa se yleisnäkymä */}
-      <p>AttendanceView</p>
+      <h1>{course ? course.name : "Placeholder name"}</h1>
+      <img src="/images/qr_code.svg" alt="" />
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <h2>Present student:</h2>
+        <div style={{ display: "flex", gap: "8px", flexWrap: "wrap" }}>
+          {presentStudent.map((student) => (
+            <button
+              key={student.studentID}
+              id={student.studentID}
+              onClick={() => {
+                presentToAbsent(student);
+              }}
+            >
+              {student.firstName} {student.lastName}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <h2>Current users:</h2>
+        <div style={{ display: "flex", gap: "8px", flexWrap: "wrap" }}>
+          {unmarkedStudent.map((student) => (
+            <button
+              key={student.studentID}
+              id={student.studentID}
+              onClick={() => {
+                absentToPresent(student);
+              }}
+            >
+              {student.firstName} {student.lastName}
+            </button>
+          ))}
+        </div>
+      </div>
+      <button onClick={()=> {navigate("/")}}>Return</button>
     </>
   );
 };
