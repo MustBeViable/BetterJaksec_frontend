9534ab6b2f436a3087e232e8651e14672993791b
leevivl <leevi.laune@metropolia.fi>
2026-03-17 11:47:20 +0200

    add localization framework and implement to login
    add localization framework and implement to login

---
+++ BetterJaksec/src/components/LanguageSwitcher.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const languages = [
+  { code: "en", label: "English" },
+  { code: "fi", label: "Suomi" },
+];
+
+export default function LanguageSelector() {
+  const { i18n } = useTranslation();
+
+  const handleChange = (e) => {
+    const selectedLang = e.target.value;
+    i18n.changeLanguage(selectedLang);
+    localStorage.setItem("lang", selectedLang);
+  };
+
+  return (
+    <select value={i18n.language} onChange={handleChange}>
+      {languages.map((lang) => (
+        <option key={lang.code} value={lang.code}>
+          {lang.label}
+        </option>
+      ))}
+    </select>
+  );
+}
